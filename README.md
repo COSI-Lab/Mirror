426f658db6e1795b2043b01cb017367e56372666
mahonec <mahonec@clarkson.edu>
2022-01-21 15:32:50 -0500

    added missing templates and refactored static files folder

--- README.md
+++ README.md
@@ -4,9 +4,11 @@ WIP monolithic software for [mirror](https://mirror.clarkson.edu) that handles
 - [x] Defining what projects we host from a centralized config
 - [x] Recording nginx bandwidth per repo
 - [ ] Recording rsync bandwidth
+- [ ] Recording tranmission bittorrent bandwidth
 - [ ] Recording total network bandwidth
 - [ ] Exposing nginx bandwidth per repo
 - [ ] Exposing rsync bandwidth
+- [ ] Exposing tranmission bittorrent bandwidth
 - [ ] Exposing total network bandwidth
 - [x] Mirror map of real time downloads
 - [ ] Periodically syncing projects
--- webserver.go
+++ webserver.go
@@ -1,16 +1,55 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
+var tmpls *template.Template
+
+func handleIndex(w http.ResponseWriter, r *http.Request) {
+	tmpls.ExecuteTemplate(w, "index.gohtml", "")
+}
+
+func handleMap(w http.ResponseWriter, r *http.Request) {
+	tmpls.ExecuteTemplate(w, "map.gohtml", "")
+}
+
+func handleDistributions(w http.ResponseWriter, r *http.Request) {
+	tmpls.ExecuteTemplate(w, "distributions.gohtml", "chris")
+}
+
+func handleHistory(w http.ResponseWriter, r *http.Request) {
+	tmpls.ExecuteTemplate(w, "history.gohtml", "")
+}
+
+func handleSoftware(w http.ResponseWriter, r *http.Request) {
+	tmpls.ExecuteTemplate(w, "software.gohtml", "")
+}
+
+func handleStatistics(w http.ResponseWriter, r *http.Request) {
+	tmpls.ExecuteTemplate(w, "statistics.gohtml", "")
+}
+
 func InitWebserver() error {
+	var err error
+	tmpls, err = template.ParseGlob("templates/*")
+
+	if err == nil {
+		log.Println("[INFO] Webserver", tmpls.DefinedTemplates())
+		return err
+	} else {
+		log.Println("\x1B[31m[Error]\x1B[0m InitWebserver", err)
+		tmpls = nil
+	}
+
 	return nil
 }
 
+// Logs request Method and request URI
 func loggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.Println("[INFO]", r.Method, r.RequestURI)
@@ -21,7 +60,16 @@ func loggingMiddleware(next http.Handler) http.Handler {
 func HandleWebserver(entries chan *LogEntry) {
 	r := mux.NewRouter()
 	r.Use(loggingMiddleware)
-	HandleMap(r.PathPrefix("/map").Subrouter(), entries)
+	r.HandleFunc("/", handleIndex)
+	MapRouter(r.PathPrefix("/map").Subrouter(), entries)
+
+	r.HandleFunc("/map", handleMap)
+	r.HandleFunc("/distributions", handleDistributions)
+	r.HandleFunc("/software", handleSoftware)
+	r.HandleFunc("/history", handleHistory)
+	r.HandleFunc("/stats", handleStatistics)
+
+	r.PathPrefix("/").Handler(http.FileServer(http.Dir("static")))
 
 	// Serve on 8080
 	l := &http.Server{
