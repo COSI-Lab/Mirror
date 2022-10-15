39142bbc1aafe1448d57b93b2b586aea01b1dfb1
MasterManiac980 <trainboy980@gmail.com>
2022-10-15 18:37:29 -0400

    finished sticky table of contents and made toc sections collapsable

--- static/js/projects.js
+++ static/js/projects.js
@@ -1,11 +1,15 @@
 // TODO: Make a working collapsable
 
-var collapsable = document.getElementById("collapsable"); // TODO: set id of "linux distributions", "software", and "miscellaneous" to collapsable in toc after making the following for loop work
+var collapsable = document.getElementsByClassName("toc-heading"); // TODO: set id of "linux distributions", "software", and "miscellaneous" to collapsable in toc after making the following for loop work
 
 for (let i = 0; i < collapsable.length; i++) {
-    collapsable[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-
-    })
-    
+  collapsable[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  })
 }
