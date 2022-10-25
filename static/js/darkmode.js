ae9b4e7056a40792b55c2ad3914afe29523a9f82
carykees98 <trainboy980@gmail.com>
2022-10-25 14:36:18 -0400

    dm now changes font color to assist with contrast issues on both light and dark mode

--- static/js/darkmode.js
+++ static/js/darkmode.js
@@ -2,24 +2,48 @@ import { prefersDark } from './checkdark.js'
 
 var doc = document.getElementsByTagName('HTML')
 
+var mainLinks = document.getElementsByTagName('MAIN')[0].getElementsByTagName('A')
+
+console.log(mainLinks.length)
+
 var modeSwapButton = document.getElementById('darkmode_button')
 
 if (prefersDark) {
 	modeSwapButton.className = 'darkmode-button-after'
+
+	for (let i = 0; i < mainLinks.length; i++) {
+		mainLinks[i].classList.add('main-link-dark')
+	}
 }
 else {
 	modeSwapButton.className = 'darkmode-button'
+
+	for (let i = 0; i < mainLinks.length; i++) {
+		mainLinks[i].classList.add('main-link-light')
+	}
 }
 
 modeSwapButton.addEventListener('click', function () {
 	if (doc[0].classList.contains('lightmode-back')) {
 		doc[0].className = 'darkmode-back'
 		modeSwapButton.className = 'darkmode-button-after'
+
+		for (let i = 0; i < mainLinks.length; i++) {
+			mainLinks[i].classList.remove('main-link-light')
+			mainLinks[i].classList.add('main-link-dark')
+		}
+
 		setDarkState('true')
 	}
 	else if (doc[0].classList.contains('darkmode-back')) {
 		doc[0].className = 'lightmode-back'
 		modeSwapButton.className = 'darkmode-button'
+
+		for (let i = 0; i < mainLinks.length; i++) {
+			mainLinks[i].classList.remove('main-link-dark')
+			mainLinks[i].classList.add('main-link-light')
+		}
+
 		setDarkState('false')
 	}
 })
