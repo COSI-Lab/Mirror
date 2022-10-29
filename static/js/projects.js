42f008149d6958068282fe8f6bf2f761296c9e52
Cary Keesler <44330591+carykees98@users.noreply.github.com>
2022-10-29 15:14:25 -0400

    Project logos (#34)

--- static/js/projects.js
+++ static/js/projects.js
@@ -6,8 +6,32 @@ for (let i = 0; i < collapsable.length; i++) {
     var content = this.nextElementSibling;
     if (content.style.display === "block") {
       content.style.display = "none";
-    } else {
+    }
+    else {
       content.style.display = "block";
+    }
+  })
+}
+
+var imageTemplates = document.getElementsByTagName("TEMPLATE")
+var imageContainers = document.getElementsByClassName("icon-container")
+var imagesLoaded = false
+
+if (window.innerWidth > 800 && !imagesLoaded) {
+  for (let index = 0; index < imageTemplates.length; index++) {
+    var image = imageTemplates[index].content.cloneNode(true)
+    imageContainers[index].appendChild(image)
+  }
+  imagesLoaded = true
+}
+else {
+  window.addEventListener('resize', function () {
+    if (window.innerWidth > 800 && !imagesLoaded) {
+      for (let index = 0; index < imageTemplates.length; index++) {
+        var image = imageTemplates[index].content.cloneNode(true)
+        imageContainers[index].appendChild(image)
+      }
+      imagesLoaded = true
     }
   })
 }
