0102e8e5c8072049aa3e1b9f92bbea0d76abe1eb
carykees98 <trainboy980@gmail.com>
2022-11-02 20:48:23 -0400

    made mobile navbar larger

--- static/js/mobile-navbar.js
+++ static/js/mobile-navbar.js
@@ -1,8 +1,14 @@
 const navMenu = document.getElementById("nav_menu")
 const navToggleButton = document.getElementById("nav_collapse_button")
+const navToggleText = navToggleButton.firstChild
 
 navToggleButton.addEventListener('click', function () {
-	navToggleButton.classList.toggle("navbutton-expanded")
+	if (navToggleText.textContent === "»") {
+		navToggleText.textContent = "«"
+	}
+	else{
+		navToggleText.textContent = "»"
+	}
 	if (navMenu.classList.contains("navmenu-collapsed")) {
 		navMenu.classList.add("navmenu-expanded")
 		navMenu.classList.remove("navmenu-collapsed")
